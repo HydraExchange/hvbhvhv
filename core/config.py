5aa98a7c8002ac3b4dda67fb3ea124f9d465a318
HydraExchange <rmcmike101@gmail.com>
2026-09-05 12:13:12 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,20 +1,27 @@
+```python
 import pathlib
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from pydantic_settings import BaseSettings
 
 def get_path_env() -> pathlib.Path:
     return pathlib.Path(__file__).resolve().parent.parent / ".env"
 
+
 class Settings(BaseSettings):
-    bot_token:8258401803:AAF0Gv5dftdP5v97G_yG45Lqv6NmcZlBEao
-    ADMIN_ID:7861322479
-    class Config:
-        env_file = get_path_env()
-        env_file_encoding = "utf-8"
-        case_sensitive = False
+    BOT_TOKEN: str = "8258401803:AAEHZTCKEa74IaxtvSlASJEnvxyODW4-sAI"
+    ADMIN_ID: int = 7861322479
+
+    model_config = SettingsConfigDict(
+        env_file=get_path_env(),
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
 
     def get_db_url(self) -> str:
         db_path = pathlib.Path(__file__).resolve().parent.parent / "db" / "base.db"
-        return db_path
-    
+        return str(db_path)
+
+
 settings = Settings()
+```
